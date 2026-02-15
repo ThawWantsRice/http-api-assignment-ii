2243f5ac46bf08b8e6ee9fae8f0ba0d3bb091148
Thaw Thaw <ttt4775@rit.edu>
2026-02-15 16:57:22 -0500

    Added in POST and other needed Parts

--- src/jsonResponse.js
+++ src/jsonResponse.js
@@ -18,7 +18,8 @@ const respondJSON = (request, response, status, object) => {
 
   // HEAD requests don't get a body back, just the metadata.
   // So if the user made one, we don't want to write the body.
-  if(request.method !== 'HEAD') {
+  // 204 for "no content responses"
+  if (request.method !== 'HEAD' && status !== 204) {
     response.write(content);
   }
 
@@ -26,18 +27,48 @@ const respondJSON = (request, response, status, object) => {
 };
 
 const getUsers = (request, response) => {
-  // json object to send
-  const responseJSON = {
-    users,
-  };
-
-  //notRealThing
-
   // return 200 with message
-  return respondJSON(request, response, 200, responseJSON);
+  return respondJSON(request, response, 200, users);
 };
 
-const notFound = (request, response) => {
+const addUser = (request, response) => {
+  const responseJSON = {
+    message: 'Name and age are both required'
+  };
+
+  // grab name and age out of request.body for convenience
+  // If either name or age do not exist in the request,
+  // they will be set to undefined
+  const { name, age } = request.body;
+
+  if (!name || !age) {
+    responseJSON.id = 'addUserMissingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+  console.log(request.body);
+
+  //204 = no content
+  let responseCode = 204;
+
+  if (!users[name]) {
+    responseCode = 201;
+    users[name] = {
+      name: name,
+    };
+  }
+
+  //Updates go here Hint Hint Nudge Nudge API Project
+  users[name].age = age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, 201, responseJSON);
+  }
+
+  return respondJSON(request, response, responseCode, {});
+}
+
+const notReal = (request, response) => {
   // create error message for response
   const responseJSON = {
     message: 'The page you are looking for was not found.',
@@ -50,6 +81,6 @@ const notFound = (request, response) => {
 
 module.exports = {
   getUsers,
-
-  notFound,
+  addUser,
+  notReal,
 };
--- src/server.js
+++ src/server.js
@@ -6,20 +6,39 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
   '/': htmlHandler.getIndex,
-  '/style.css': htmlHandler.getCSS, 
+  '/style.css': htmlHandler.getCSS,
+  '/getUsers': jsonHandler.getUsers,
+  '/addUser': jsonHandler.addUser,
 
-    notFound: jsonHandler.notFound,
+  notReal: jsonHandler.notReal,
 };
+
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    request.body = JSON.parse(body);
+    handler(request, response);
+  });
+}
 
 const onRequest = (request, response) => {
   const protocol = request.connection.encrypted ? 'https' : 'http';
   const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
+  if (parsedUrl.pathname === '/addUser' && request.method === 'POST') {
+    return parseBody(request, response, jsonHandler.addUser);
+  }
+
   if (urlStruct[parsedUrl.pathname]) {
     return urlStruct[parsedUrl.pathname](request, response);
   }
-  
-  return urlStruct.notFound(request, response);
+
+  return urlStruct.notReal(request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
